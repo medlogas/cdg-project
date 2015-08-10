3606787684af61230db597b4f1577f6b2e7f87ef
harbouj <mohammed.elharbouj@gmail.com>
2015-08-10 18:31:40 +0100

    create project View

--- js/app.js
+++ js/app.js
@@ -2,5 +2,11 @@ var app = angular.module('app', ['ngRoute']);
 
 app.config(['$routeProvider', function($routeProvider){
 
+	$routeProvider.when('/projets', {
+		templateUrl: "views/projets.html",
+		controller: "projetsController",
+		controllerAs: "main"
+	}).
+	otherwise({redirectTo: '/projets'});
 
 }]);
