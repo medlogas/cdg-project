0c4eb30045072afa322acb436f901e2732477067
harbouj <mohammed.elharbouj@gmail.com>
2015-08-12 18:35:43 +0100

    adding new features

--- js/app.js
+++ js/app.js
@@ -1,12 +1,33 @@
-var app = angular.module('app', ['ngRoute']);
+var app = angular.module('app', ['ui.router']);
 
-app.config(['$routeProvider', function($routeProvider){
+app.config(['$stateProvider', function($stateProvider){
 
-	$routeProvider.when('/projets', {
-		templateUrl: "views/projets.html",
-		controller: "projetsController",
-		controllerAs: "main"
-	}).
-	otherwise({redirectTo: '/projets'});
+	$stateProvider
+		.state("projets", { 
+			url:"/projets", 
+			templateUrl: "views/projets.html",
+			controller: "projetsController",
+			controllerAs: "main"
+		})
+		.state("add", {
+				// parent: "projets",
+			url: "/add_projet",
+			templateUrl: "views/add.html",
+			controller: "addController"	
+		})
 
-}]);
+		.state("companies", {
+			url:"/companies",
+			template:"<h3>Companies</h3>"
+		})
+
+		.state("dashbord", {
+			url:"/dashbord",
+			template: "<h3><i class='fa fa-tachometer'></i> Dashboard</h3>"
+		})
+
+
+}]).run(function($rootScope, $state) {
+      $rootScope.$state = $state;
+      $rootScope.collapse = true;
+    });
